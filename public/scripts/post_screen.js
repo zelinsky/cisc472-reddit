ab30d249c6c98591bdbbd8b8b8322f03c9f48392
Braunn <augen@udel.edu>
2019-11-14 09:02:22 -0800

    added voting checks

--- public/scripts/post_screen.js
+++ public/scripts/post_screen.js
@@ -1,63 +1,108 @@
-// import below doesnt work!
-//import { Firestore } from "@google-cloud/firestore";
-
 /*
 TO DO
-- request data from db
-- add more functionality to fillPostPAge Function (handle other content types)
-  -> confirm with matt what we want to store
-- have voting function check if user has voted
-- fetch post fields then fill
-- send votes
-- fetch comments (nested)
-- send comments
-*/
+- add more content types to fillPostPAge 
 
-// voting listeners
-//document.getElementById("upvote").addEventListener("click", myScript);
-//document.getElementById("downvote").addEventListener("click", myScript);
+-get user 
+-have voting function check if user has voted
+
+-fetch comments (nested)
+
+-send comments
+
+-get post instance from previous request
+
+*/
 
 $( document ).ready(function() {
   
-  //var post_instance = firebase.firestore().collection("subreddits").collection("websec").collection("posts").doc("post_exp");
-  var test = firebase.firestore().collection("subreddits").doc("websec").collection("posts").doc("post_example").get();
-  console.log(test);
-  /*
-  if(post_instance.get()){
-    console.log(post_instance.data());
-  }
-  */
-
-  //TEST FUNCTIONS HERE
-  //fillPostPage("test post","can you see the content?","text","nathan a","DNE subreddit");
-  //fillPostPage("test post",Firestore.subreddit.toString(),"text","nathan a","DNE subreddit");
-  //fillPostPage(post_instance.title)
+  // get instance of post;
+  var cur_sub = "websec";
+  var post_key = "post_exp";
+  var user_key = "bobr";
+  var post_inst = firebase.firestore().collection("subreddits").doc(cur_sub).collection("posts").doc(post_key);
+  var c_user = firebase.firestore().collection("users").doc(user_key);
+  
+  // handler for retrieving post
+  post_inst.get().then(function(doc) {
+      if (doc.exists) {
+          var data = doc.data();
+          console.log("Document data:",data.title);
+          fillPostPage(data);
+      } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+      }
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+  
+  // listen for votes from server
+  post_inst.onSnapshot(function(doc) {
+    $("#votes").text(doc.data().votes);
+  });
+  
+  //listen to votes from client 
+  $("#upvote_button").click(function(){vote(1, post_inst, c_user, post_key);});
+  $("#downvote_button").click(function(){vote(-1, post_inst, c_user, post_key);});
 });
 
+function vote(newVote, post_inst, c_user, post_key){
 
-function vote(direction){
   // check if user has 1.Is logged in and 2.Has not voted already
-  if(1){
-    if(direction){
-       // update upvote count with + 1
-    }
-    else {
-      // update downvote count with - 1
-    }
-  }
+  var voteUpdate = 0; // stores value we have to add to post votes,  changes based on new vote
+  var c_user_post = c_user.collection("userPosts").doc(post_key);// if user has intacted with this post before this gets that doc
+
+  // checks if doc exists
+  c_user_post.get().then(function(doc) {
+      if (doc.exists) {
+          var oldVote = doc.data().vote;
+          // user has interacted with this post before! They either made it or voted on it
+          console.log("Previous user vote:",oldVote);
+          
+          // only update votes in post if direction doesnt match current vote
+          if(newVote != oldVote) {
+            voteUpdate = newVote - oldVote; // ensures the post is updated with the correct vote count
+            //console.log("vote update: ", voteUpdate);
+            c_user_post.update({vote : newVote}); // updates user vote 
+          }
+      } else {
+          // make new post instance and vote *******TEST PLS
+          voteUpdate = newVote;
+          
+          c_user_post.set({
+            make: false,
+            vote: voteUpdate,
+            post: post_inst._document_path
+          });
+      }
+ 
+      // update server
+      post_inst.get().then(function(doc) {
+        post_inst.update({votes: doc.data().votes + voteUpdate}); // update vote in server
+      }).catch(function(error) {
+          console.log("Error getting document:", error);
+      });
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
 }
 
-function fillPostPage(title,content,type,user_owner,subreddit){
-  var titleElement = document.getElementById("content") // replace with jQuery $("#id of element")
+function fillPostPage(postData){
+  // want to show: title,content,type,user_owner,subreddit
+  
 
   // decide how to handle content based on its type
-  switch(type){
+  switch(postData.type){
     case "text":
       // handles text content
-      titleElement.innerHTML = content;
+      $("#content").html(postData.content);
       break;
     case "link":
       //handles links
       break;
   }
+
+  // fill in metadata
+  $("#user_owner").html(postData.user);
+  $("#title").html(postData.title);
 }
