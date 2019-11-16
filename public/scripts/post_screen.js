a660b09fc79b4104982d538d9209a4d9378f5cb2
Braunn <augen@udel.edu>
2019-11-15 16:08:28 -0800

    updated post screen

--- public/scripts/post_screen.js
+++ public/scripts/post_screen.js
@@ -1,29 +1,24 @@
 /*
 TO DO
-- add more content types to fillPostPAge 
+-add more content types to fillPostPAge (optional?)
 
--get user 
--have voting function check if user has voted
+-get post and user instance from previous request
 
--fetch comments (nested)
-
--send comments
-
--get post instance from previous request
+-votes for comments (add field in post)
 
 */
 
 $( document ).ready(function() {
   
   // get instance of post;
-  var cur_sub = "websec";
-  var post_key = "post_exp";
+  var currentSub = "websec";
+  var postKey = "post_exp";
   var user_key = "bobr";
-  var post_inst = firebase.firestore().collection("subreddits").doc(cur_sub).collection("posts").doc(post_key);
-  var c_user = firebase.firestore().collection("users").doc(user_key);
+  var postInst = firebase.firestore().collection("subreddits").doc(currentSub).collection("posts").doc(postKey);
+  var currentUser = firebase.firestore().collection("users").doc(user_key);
   
   // handler for retrieving post
-  post_inst.get().then(function(doc) {
+  postInst.get().then(function(doc) {
       if (doc.exists) {
           var data = doc.data();
           console.log("Document data:",data.title);
@@ -36,24 +31,56 @@ $( document ).ready(function() {
       console.log("Error getting document:", error);
   });
   
+  // fill up comments
+  /*
+  postInst.collection("comments").get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+        addComment(getUsername(doc),doc.data().text);
+    });
+  });
+  */
+
+  //addComment("big boy", "it's me");
+  //addComment("triggerman21", "no way, it's him");
+
+  //* LISTENERS */
   // listen for votes from server
-  post_inst.onSnapshot(function(doc) {
+  postInst.onSnapshot(function(doc) {
     $("#votes").text(doc.data().votes);
   });
   
   //listen to votes from client 
-  $("#upvote_button").click(function(){vote(1, post_inst, c_user, post_key);});
-  $("#downvote_button").click(function(){vote(-1, post_inst, c_user, post_key);});
+  $("#upvote_button").click(function(){vote(1, postInst, currentUser, postKey);});
+  $("#downvote_button").click(function(){vote(-1, postInst, currentUser, postKey);});
+
+  //listen for comments from client
+  $("#comment_input").keypress(function(event){
+    var keycode = (event.keycode ? event.keycode : event.which);
+    if(keycode == '13'){
+      var newComment = $("#comment_input").val();
+      $("#comment_input").val(null);
+      
+      sendComment(currentUser,newComment,postInst);
+    }
+  })
+
+  //listen for comments from server
+  postInst.collection("comments").onSnapshot(function(snapshot){
+    snapshot.docChanges().forEach(function(change){
+      addComment(getUsername(change.doc), change.doc.data().text);
+    });
+  });
+
 });
 
-function vote(newVote, post_inst, c_user, post_key){
+function vote(newVote, postInst, currentUser, postKey){
 
   // check if user has 1.Is logged in and 2.Has not voted already
   var voteUpdate = 0; // stores value we have to add to post votes,  changes based on new vote
-  var c_user_post = c_user.collection("userPosts").doc(post_key);// if user has intacted with this post before this gets that doc
+  var currentUser_post = currentUser.collection("userPosts").doc(postKey);// if user has intacted with this post before this gets that doc
 
   // checks if doc exists
-  c_user_post.get().then(function(doc) {
+  currentUser_post.get().then(function(doc) {
       if (doc.exists) {
           var oldVote = doc.data().vote;
           // user has interacted with this post before! They either made it or voted on it
@@ -63,22 +90,22 @@ function vote(newVote, post_inst, c_user, post_key){
           if(newVote != oldVote) {
             voteUpdate = newVote - oldVote; // ensures the post is updated with the correct vote count
             //console.log("vote update: ", voteUpdate);
-            c_user_post.update({vote : newVote}); // updates user vote 
+            currentUser_post.update({vote : newVote}); // updates user vote 
           }
       } else {
           // make new post instance and vote *******TEST PLS
           voteUpdate = newVote;
           
-          c_user_post.set({
+          currentUser_post.set({
             make: false,
             vote: voteUpdate,
-            post: post_inst._document_path
+            post: postInst.path
           });
       }
  
       // update server
-      post_inst.get().then(function(doc) {
-        post_inst.update({votes: doc.data().votes + voteUpdate}); // update vote in server
+      postInst.get().then(function(doc) {
+        postInst.update({votes: doc.data().votes + voteUpdate}); // update vote in server
       }).catch(function(error) {
           console.log("Error getting document:", error);
       });
@@ -103,6 +130,48 @@ function fillPostPage(postData){
   }
 
   // fill in metadata
-  $("#user_owner").html(postData.user);
+  // parse path to get username
+  var tmp = postData.user.path.split("/");
+  var username = tmp[tmp.length-1];
+
+  $("#user_owner").html(username);
   $("#title").html(postData.title);
+}
+
+function sendComment(user, text, post){
+  // user should be reference path
+  // post should be db object
+  //console.log(post.collection("comments").doc().getId());
+  
+  // send to post collection
+  post.collection("comments").add({
+    user: user,
+    text: text 
+  }) 
+  .then(function(doc){
+    // send to user collection
+    user.collection("userComments").add({
+      commentPath: doc,
+      made: true,
+      vote: 0
+    });
+  });
+}
+
+function addComment(user, newComment){
+  //$("#comments").text(newComment);
+  //$("#comments").text($("#comments").text().concat("\n", user,": ",newComment,"\n"));
+  
+  var userText = $("<dt style='font-weight:bold'></dt>").text(user);
+  var commentText = $("<dd></dd>").text(newComment);
+
+  $("#comments").prepend(userText, commentText);
+}
+
+function getUsername(ref){
+  // gets the username from the reference path (returns a string)
+  var tmp = ref.data().user.path.split("/");
+  var username = tmp[tmp.length-1];
+  console.log(username);
+  return  username;
 }
